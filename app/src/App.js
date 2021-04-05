a2f53ef71e04f7d53b4bb3c02e2c34c859dfb895
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-04-05 16:34:54 +0100

    add app theme

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,20 @@
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 import NavLinks from './routing/navLinks.json';
 import Login from './containers/login/Login';
 import MainContainer from './containers/MainContainer';
-import './App.css';
+import styles from './App.module.css';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path={NavLinks.login.link} component={Login} />
-        <Route path={NavLinks.home.link} component={MainContainer} />
-      </Switch>
+    <div className={styles.App}>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path={NavLinks.login.link} component={Login} />
+          <Route path={NavLinks.home.link} component={MainContainer} />
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ app/src/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#F4F2F3'
+    },
+    secondary: {
+      main: '#A7C6DA'
+    },
+    background: {
+      default: '#F49F0A'
+    },
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#06070E'
+    }
+  }
+});
+
+export default theme;
