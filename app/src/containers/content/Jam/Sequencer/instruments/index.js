88cbc19574108aee20b96613b7b7463cd4984351
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-04-09 16:45:21 +0100

    fix TestSynth instrument notes

--- app/src/containers/content/Jam/Sequencer/instruments/index.js
+++ app/src/containers/content/Jam/Sequencer/instruments/index.js
@@ -3,6 +3,6 @@ import testSynth from './testSynth';
 export default {
   TestSynth: {
     instrument: testSynth,
-    notes: ['C3, C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3'],
+    notes: ['C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3'],
   },
 };
